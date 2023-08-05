5fb35f422bd7e164ec5296ada72d5b421c0fc25e
dispoclavier <dispoclavier@orange.fr>
2023-08-06 00:56:02 +0200

    Update generate-math-table.pl

--- linux-chromeos/outils/generate-math-table.pl
+++ linux-chromeos/outils/generate-math-table.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 # 2023-07-19T1747+0200
 # 2023-07-23T1447+0200
-# 2023-08-06T0010+0200
+# 2023-08-06T0052+0200
 # Last modified: See datestamp above.
 # 
 # Generates an HTML table of math symbols, based
@@ -24,6 +24,13 @@ use feature 'unicode_strings';
 use open ":std", ":encoding(UTF-8)";
 
 
+## Character names localization
+# my $names_file_path       = 'names/NamesList.txt';
+my $names_file_path       = 'names/ListeNoms.txt';
+# my $descriptors_file_path = '';
+my $descriptors_file_path = 'names/Udescripteurs.txt';
+
+
 my $file_path = 'Compose.yml';
 open( INPUT, '<', $file_path ) or die $!;
 print( "Opened file $file_path.\n" );
@@ -34,12 +41,13 @@ print( "Opened file $output_path.\n" );
 
 print( "Processing math symbols from $file_path to $output_path.\n" );
 my $math_flag = !1;
-my $table_header_1 = 'Symbole';
+my $table_header_1 = 'Caractère';
 my $table_header_2 = 'Séquence de composition';
 my $table_header_3 = 'Identifiant Unicode';
 my $line = "<figure class=\"wp-block-table alignwide math\"><table><thead><tr><th colspan=\"2\" class=\"has-text-align-left\" data-align=\"left\">$table_header_1</th><th class=\"has-text-align-left\" data-align=\"left\">$table_header_2</th><th class=\"has-text-align-left\" data-align=\"left\">$table_header_3</th></tr></thead><tbody>";
-print OUTPUT "$line";
-print OUTPUT "\n";
+print OUTPUT "$line\n";
+my ( $str, $cp, $descrip, $tooltip, $anchor, @anchors, $regex, $test, $index );
+
 while ( my $line = <INPUT> ) {
 	if ( $line =~ /START_MATH/ ) {
 		$math_flag = !0;
@@ -88,8 +96,55 @@ while ( my $line = <INPUT> ) {
 				$line =~ s/ {2,}/ /g;
 				$line =~ s/> </></g;
 				$line =~ s/<(.+?)>/$1/g;
-				$line =~ s/^(.+?) : "(.+?)" U(20[DE][0-9A-F]) # (.+)/<tr><td>◌$2<\/td><td>U+$3<\/td><td>$1<\/td><td>$4<\/td><\/tr>/;
-				$line =~ s/^(.+?) : "(.+?)" U([0-9A-F]{4}) # (.+)/<tr><td>$2<\/td><td>U+$3<\/td><td>$1<\/td><td>$4<\/td><\/tr>/;
+
+				# Anchors and localized tooltips:
+				$line    =~ m/^.+ : +"(.+?)"/u;
+				$str     = $1;
+				$cp      = ord( $str );
+				$cp      = sprintf( "%X", $cp );
+				$cp      =~ s/^(..)$/00$1/;
+				$cp      =~ s/^(...)$/0$1/;
+				$descrip = '';
+				if ( $descriptors_file_path ) {
+					open( DESCRIP, '<', $descriptors_file_path ) or die $!;
+					while ( my $des_line = <DESCRIP> ) {
+						if ( $des_line =~ /U$cp;/ ) {
+							chomp( $des_line );
+							$des_line =~ s/^.+; (.+)$/$1/;
+							$descrip = $des_line;
+							last;
+							close( DESCRIP );
+						}
+					}
+				}
+				unless ( $descrip ) {
+					open( NAMES, '<', $names_file_path ) or die $!;
+					while ( my $name_line = <NAMES> ) {
+						if ( $name_line =~ /^$cp\t/ ) {
+							chomp( $name_line );
+							$name_line =~ s/^.+\t(.+)$/$1/;
+							$descrip = $name_line;
+							last;
+							close( NAMES );
+						}
+					}
+				}
+				$tooltip = $descrip . " U+$cp";
+				$anchor  = 'U+' . $cp;
+				$regex   = quotemeta( $anchor );
+				$index   = 0;
+				if ( grep( /^$regex$/, @anchors ) ) {
+					$test = $regex;
+					while ( grep( /^$test$/, @anchors ) ) {
+						++$index;
+						$test = $regex . '_' . $index;
+					}
+					$anchor = $anchor . '_' . $index;
+				}
+				push( @anchors, $anchor );
+				
+				$line =~ s/^(.+?) : "(.+?)" U(20[DE][0-9A-F]) # (.+)/<tr id="$anchor"><td title="$tooltip"><a href="#$anchor">◌$2<\/a><\/td><td>U+$3<\/td><td>$1<\/td><td>$4<\/td><\/tr>/;
+				$line =~ s/^(.+?) : "(.+?)" U([0-9A-F]{4}) # (.+)/<tr id="$anchor"><td title="$tooltip"><a href="#$anchor">$2<\/a><\/td><td>U+$3<\/td><td>$1<\/td><td>$4<\/td><\/tr>/;
 				print OUTPUT "$line";
 			}
 		}
