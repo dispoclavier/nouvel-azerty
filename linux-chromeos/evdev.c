0559e8ff5b8b34ce91f775624f6293e0a8f98973
dispoclavier <dispoclavier@orange.fr>
2023-08-16 19:55:51 +0200

    Update evdev.c

--- linux-chromeos/evdev.c
+++ linux-chromeos/evdev.c
@@ -1,4 +1,4 @@
-//                       Date: 2023-08-11T0824+0200
+//                       Date: 2023-08-16T1955+0200
 //        Operating file name: evdev
 //                   Encoding: UTF-8
 //                       Type: text/XKB configuration
@@ -21,9 +21,6 @@
 //                 (oncoming)  kbfrCAms French Canada multilingual semiautomatic keyboard layout
 //                 (oncoming)  kbfrCAs  French Canada semiautomatic keyboard layout
 //                 (oncoming)  kbfrCHs  French Switzerland semiautomatic keyboard layout
-//                             kbfrFRhw French France hybrid AZERTY semiautomatic keyboard layout
-//                             kbfrFRhy French France hybrid QWERTZ semiautomatic keyboard layout
-//                             kbfrFRhz French France hybrid QWERTY semiautomatic keyboard layout
 //                             kbfrFRr  French France remapped semiautomatic keyboard layout
 //                             kbfrFRs  French France semiautomatic keyboard layout
 //                             kbfrPFs  French Polynesia semiautomatic keyboard layout
@@ -122,10 +119,10 @@ default xkb_keycodes "evdev" {
 
         <BKSP> =  22;  // was <BKSP>   Overridden if Backspace on right Windows for desktop.
 
-  //    <BKSP> = 134;  // was <RWIN>   Backspace on right Windows for desktop.
-  //    <SCLK> = 135;  // was <COMP>   Backspace on right Windows for desktop: Deactivates the menu key.
-        <RWIN> = 134;  // was <RWIN>   Common mappings for desktop and laptop.
-        <COMP> = 135;  // was <COMP>   Common mappings for desktop and laptop.
+        <BKSP> = 134;  // was <RWIN>   Backspace on right Windows for desktop.
+        <SCLK> = 135;  // was <COMP>   Backspace on right Windows for desktop: Deactivates the menu key.
+  //    <RWIN> = 134;  // was <RWIN>   Common mappings for desktop and laptop.
+  //    <COMP> = 135;  // was <COMP>   Common mappings for desktop and laptop.
 
   // Other left-hand functional keys:
 
