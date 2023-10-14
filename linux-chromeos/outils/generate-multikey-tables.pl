8911e1b884ad583a6591f1492cc62ccd80a36e40
dispoclavier <dispoclavier@orange.fr>
2023-10-14 18:29:21 +0200

    Update generate-multikey-tables.pl

--- linux-chromeos/outils/generate-multikey-tables.pl
+++ linux-chromeos/outils/generate-multikey-tables.pl
@@ -3,7 +3,9 @@
 # 2023-08-07T1723+0200
 # 2023-08-18T0344+0200
 # 2023-08-20T0243+0200
-# Last modified: See datestamp above.
+# 2023-10-07T1220+0200
+# 2023-10-14T1823+0200
+# = last modified.
 #
 # Generates HTML tables of Multi_key from multi-key sequences in `Compose.yml`.
 #
@@ -13,7 +15,9 @@
 # target locale as configured under `## Character names localization`.
 # `ListeNoms.txt` is used for characters missing from `Udescripteurs.txt`.
 #
-# The output is designed for use in WordPress.
+# The output is designed for use in WordPress, where {{{anrghg-classes}}} can
+# be replaced with additional CSS classes, as well as {{{anrghg-value}}} with
+# anything, classes too in this file, using the A.N.R.GHG Publishing Toolkit.
 #
 # Using old-style file handles.
 use warnings;
@@ -56,7 +60,7 @@ my $parse_on       = !1;
 my $table_header_1 = 'Caractère';
 my $table_header_2 = 'Séquence de composition';
 my $table_header_3 = 'Identifiant Unicode';
-my $start_tags     = "<figure class=\"wp-block-table alignwide multikey\"><table><thead><tr><th colspan=\"2\" class=\"has-text-align-left\" data-align=\"left\">$table_header_1</th><th class=\"has-text-align-left\" data-align=\"left\">$table_header_2</th><th class=\"has-text-align-left\" data-align=\"left\">$table_header_3</th></tr></thead><tbody>\n";
+my $start_tags     = "<figure class=\"wp-block-table alignwide multikey {{{anrghg-classes}}} {{{anrghg-value}}}\"><table><thead><tr><th colspan=\"2\" class=\"has-text-align-left\" data-align=\"left\">$table_header_1</th><th class=\"has-text-align-left\" data-align=\"left\">$table_header_2</th><th class=\"has-text-align-left\" data-align=\"left\">$table_header_3</th></tr></thead><tbody>\n";
 my $end_tags       = "</tbody></table></figure>\n";
 print WHOLEOUTPUT $start_tags;
 print OUTPUT $start_tags;
