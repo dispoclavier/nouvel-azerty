9361c32c08f5bdbf913e645081f83b04e070b5fb
dispoclavier <dispoclavier@orange.fr>
2023-10-27 14:57:09 +0200

    Update comp.sh

--- linux-chromeos/outils/comp.sh
+++ linux-chromeos/outils/comp.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 # Compile .xkb
 # 2023-01-14T1934+0100
-# 2023-03-24T0156+0100
-# 2023-03-24T0441+0100
-# 2023-08-19T1949+0200
-# 2023-10-24T1930+0200
+# Last modified:
+# 2023-10-27T1433+0200
 
+cd $(dirname "$0")
 if [[ ! -d "Variantes" ]]; then
 	mkdir Variantes
 fi
 cd Variantes
-echo "o = Dispositions ordinaires."
+echo "o = Disposition ordinaire."
 echo "w = Effacement arrière sur Windows droite."
 echo "s = Effacement arrière sur Windows droite sans Menu."
 echo "m = Effacement arrière sur Menu."
 echo "c = Effacement arrière sur Contrôle droite."
 echo "a = Permuter VerrCap et touche ISO pour claviers ANSI."
 echo "b = Permuter VerrCap et touche ISO, Effacement arrière sur Menu."
-read -p "o, w, s, m, c, a, b?   " re
+read -p "o, w, s, m, c, a, b ?   " re
 case $re in
-	[oO]) suffix="" ;;
-	[wW]) suffix="-win" ;;
-	[sS]) suffix="-win-sans" ;;
-	[mM]) suffix="-menu" ;;
-	[cC]) suffix="-ctrl" ;;
-	[aA]) suffix="-ansi" ;;
-	[bB]) suffix="-ansi-menu" ;;
+	[oO]) suffix="";;
+	[wW]) suffix="-win";;
+	[sS]) suffix="-win-sans";;
+	[mM]) suffix="-menu";;
+	[cC]) suffix="-ctrl";;
+	[aA]) suffix="-ansi";;
+	[bB]) suffix="-ansi-menu";;
 	*) exit
 esac
 read -p  "kbfrFRs$suffix.xkb?"; xkbcomp :0  kbfrFRs$suffix.xkb; echo  "kbfrFRs$suffix.xkb compiled."
