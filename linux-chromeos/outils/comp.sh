76bca78b35213b2a75efbd42493b3ba5ac47459e
dispoclavier <dispoclavier@orange.fr>
2023-10-29 07:50:48 +0100

    Update comp.sh
    Sous-variantes : Ajouter des sous-variantes avec VerrCap et touche ISO permutées et avec Effacement arrière sur Menu sans permutation.

--- linux-chromeos/outils/comp.sh
+++ linux-chromeos/outils/comp.sh
@@ -2,32 +2,34 @@
 # Compile .xkb
 # 2023-01-14T1934+0100
 # Last modified:
-# 2023-10-28T1143+0200
+# 2023-10-29T0642+0100
 
 cd $(dirname "$0")
 if [[ ! -d "Variantes" ]]; then
 	mkdir Variantes
 fi
 cd Variantes
-echo "o = Disposition ordinaire."
-echo "w = Effacement arrière sur Windows droite."
-echo "s = Effacement arrière sur Windows droite sans Menu."
-echo "m = Effacement arrière sur Menu, Menu sur Effacement arrière."
-echo "n = Effacement arrière sur Menu seulement, sans permutation."
-echo "c = Permuter Effacement arrière et Contrôle droite."
-echo "a = Permuter VerrCap et touche ISO pour claviers ANSI."
-echo "b = Permuter VerrCap et touche ISO, Effacement arrière sur Menu."
-read -p "o, w, s, m, n, c, a, b ?   " re
+echo "      o = Disposition ordinaire."
+echo "      w = Effacement arrière sur Windows droite."
+echo "     ws = Effacement arrière sur Windows droite sans Menu."
+echo "      m = Effacement arrière sur Menu, Menu sur Effacement arrière."
+echo "     ms = Effacement arrière sur Menu seulement, sans permutation."
+echo "      c = Permuter Effacement arrière et Contrôle droite."
+echo "      a = Permuter VerrCap et touche ISO pour claviers ANSI."
+echo "     am = Permuter VerrCap et touche ISO, et Effacement arrière et Menu."
+echo "    ams = Permuter VerrCap et touche ISO, Effacement arrière sur Menu."
+read -p "          o, w, ws, m, ms, c, a, am, ams ?   " re
 case $re in
-	[oO]) suffix="";;
-	[wW]) suffix="-win";;
-	[sS]) suffix="-win-sans";;
-	[mM]) suffix="-menu";;
-	[nN]) suffix="-menu-sans";;
-	[cC]) suffix="-ctrl";;
-	[aA]) suffix="-ansi";;
-	[bB]) suffix="-ansi-menu";;
-	*) exit
+	o)   suffix="";;
+	w)   suffix="-win";;
+	ws)  suffix="-win-sans";;
+	m)   suffix="-menu";;
+	ms)  suffix="-menu-sans";;
+	c)   suffix="-ctrl";;
+	a)   suffix="-ansi";;
+	am)  suffix="-ansi-menu";;
+	ams) suffix="-ansi-menu-sans";;
+	*)   exit
 esac
 read -p  "kbfrFRs$suffix.xkb?"; xkbcomp :0  kbfrFRs$suffix.xkb; echo  "kbfrFRs$suffix.xkb compiled."
 read -p  "kbbrFRs$suffix.xkb?"; xkbcomp :0  kbbrFRs$suffix.xkb; echo  "kbbrFRs$suffix.xkb compiled."
