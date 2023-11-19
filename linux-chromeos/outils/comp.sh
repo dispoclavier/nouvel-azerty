a0b9d9a2d2e7219a5abdc2ca6ca5637a77b36db4
dispoclavier <dispoclavier@orange.fr>
2023-11-19 18:58:12 +0100

    Update comp.sh

--- linux-chromeos/outils/comp.sh
+++ linux-chromeos/outils/comp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Compile .xkb
 # 2023-01-14T1934+0100
-# Last modified:
-# 2023-10-29T0642+0100
+# 2023-11-19T1857+0100
+# = last modified.
 
 cd $(dirname "$0")
 if [[ ! -d "Variantes" ]]; then
@@ -31,10 +31,10 @@ case $re in
 	ams) suffix="-ansi-menu-sans";;
 	*)   exit
 esac
-read -p  "kbfrFRs$suffix.xkb?"; xkbcomp :0  kbfrFRs$suffix.xkb; echo  "kbfrFRs$suffix.xkb compiled."
-read -p  "kbbrFRs$suffix.xkb?"; xkbcomp :0  kbbrFRs$suffix.xkb; echo  "kbbrFRs$suffix.xkb compiled."
-read -p  "kbfrPFs$suffix.xkb?"; xkbcomp :0  kbfrPFs$suffix.xkb; echo  "kbfrPFs$suffix.xkb compiled."
-read -p  "kbfrAFs$suffix.xkb?"; xkbcomp :0  kbfrAFs$suffix.xkb; echo  "kbfrAFs$suffix.xkb compiled."
-read -p  "kbfrBEs$suffix.xkb?"; xkbcomp :0  kbfrBEs$suffix.xkb; echo  "kbfrBEs$suffix.xkb compiled."
-read -p  "kbfrFRr$suffix.xkb?"; xkbcomp :0  kbfrFRr$suffix.xkb; echo  "kbfrFRr$suffix.xkb compiled."
+read -p  "kbfrFRs$suffix.xkb?";  xkbcomp :0  kbfrFRs$suffix.xkb;  echo  "kbfrFRs$suffix.xkb compiled."
+read -p  "kbbrFRs$suffix.xkb?";  xkbcomp :0  kbbrFRs$suffix.xkb;  echo  "kbbrFRs$suffix.xkb compiled."
+read -p  "kbfrPFs$suffix.xkb?";  xkbcomp :0  kbfrPFs$suffix.xkb;  echo  "kbfrPFs$suffix.xkb compiled."
+read -p  "kbfrAFs$suffix.xkb?";  xkbcomp :0  kbfrAFs$suffix.xkb;  echo  "kbfrAFs$suffix.xkb compiled."
+read -p  "kbfrBEs$suffix.xkb?";  xkbcomp :0  kbfrBEs$suffix.xkb;  echo  "kbfrBEs$suffix.xkb compiled."
+read -p  "kbfrFRsr$suffix.xkb?"; xkbcomp :0  kbfrFRsr$suffix.xkb; echo  "kbfrFRsr$suffix.xkb compiled."
 cp kbfrFRs$suffix.xkb ../nouvel-azerty$suffix.xkb; echo "nouvel-azerty$suffix.xkb updated."
