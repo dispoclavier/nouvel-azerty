5fb563605062fa49667bcb1f468a75074fc5fb2c
dispoclavier <dispoclavier@orange.fr>
2024-08-26 09:33:19 +0200

    Update activer.sh
    * Activation : Fournir un script pour faciliter l’activation.

--- linux-chromeos/activer.sh
+++ linux-chromeos/activer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#                       Date : 2024-08-25T0656+0200
+#                       Date : 2024-08-26T0914+0200
 #                    Fichier : activer.sh
 #                   Encodage : UTF-8
 #                       Type : script Bash
@@ -151,13 +151,184 @@
 #   et éditables pour de petites personnalisations.
 #
 # L’absence des booléens dans Bash est palliée par le recours aux comparaisons
-# arithmétiques. On en profite pour inverser 0 et 1 (qui dans Bash signifient
-# vrai et faux contrairement à la plupart des autres langages où ils signifient
-# faux et vrai), dans un souci de lisibilité.
+# arithmétiques. On en profite pour inverser 0 et 1 — qui dans Bash signifient
+# vrai et faux, contrairement à la plupart des autres langages où ils
+# signifient faux et vrai — dans un souci de lisibilité.
 #
-intelligible=1 # Format XKB. Changer ce nombre pour passer au format XKM.
+intelligible=1 # Format XKB. Changer pour prendre en charge le format XKM.
 if [ "$intelligible" -eq 1 ]; then
 	mode="xkb"
 else
 	mode="xkm"
+fi
+
+function config_xim {
+	if ! im-config -m | grep -q 'xim'; then
+		echo -e "\n  ⚠  Ces dispositions de clavier fonctionnent uniquement"
+		echo      '     si le système de saisie au clavier est XIM.'
+		echo -e "\n  ⚠  Sans la X Window Input Method, toutes les séquences"
+		echo      '     multi-caractères sont en panne.'
+		echo      '     Or sans séquences multi-caractères, ces dispositions'
+		echo      '     semi-automatiques ne peuvent être opérationnelles.'
+		echo      '     Même les lettres composées qui sont dans les touches'
+		echo      '     mortes ne peuvent être saisies, étant en panne.'
+		echo -e "\n     XIM une fois sélectionnée, elle sera active"
+		echo      '     dès la prochaine session.'
+		echo -e "\n  ⚠  Avant d’installer ces dispositions de clavier,"
+		echo      '     le mieux est de commencer par activer XIM.'
+		echo -e "\n       Pour activer XIM, appuyez sur Entrée."
+		echo      '       Pour quitter, tapez q ou n puis Entrée.'
+		read -p   '    ' reponse
+		case $reponse in
+			[nNqQ])
+				exit
+			;;
+			*)
+				# Basculer vers le système de saisie au clavier XIM.
+				im-config -n xim
+				echo      '    XIM a maintenant été sélectionné.'
+			;;
+		esac
+	fi
+}
+
+function options_de_disposition {
+	config_xim
+	echo -e "\n  ⚠  Vous pouvez opter pour ce nouvel AZERTY par défaut,"
+	echo      '     ou choisir une variante et éventuellement une sous-variante.'
+	echo -e "\n  ❓  Souhaitez-vous choisir rapidement dans deux listes ?"
+	echo -e "\n       Si oui, appuyez sur Entrée."
+	echo      '       Pour quitter, tapez q ou n puis Entrée.'
+	read -p   '    ' repo
+	case $repo in
+		[nNqQ])
+			exit
+		;;
+		*)
+			echo -e "\n  ❓  Quelle variante souhaitez-vous activer ? Pour passer, faites Entrée."
+			echo      '   1  Français France disposition de clavier semi-automatique – kbfrFRs'
+			echo      '   2  Breton et français disposition de clavier semi-automatique – kbbrFRs'
+			echo      '   3  Polynésie française disposition de clavier semi-automatique – kbfrPFs'
+			echo      '   4  Afrique francophone disposition de clavier semi-automatique – kbfrAFs'
+			echo      '   5  Français Belgique disposition de clavier semi-automatique – kbfrBEs'
+			echo      '   6  Français France disposition de clavier semi-automatique redisposée – kbfrFRsr'
+			echo      '   7  Breton et français disposition de clavier semi-automatique redisposée – kbbrFRsr'
+			echo      '   8  Polynésie française disposition de clavier semi-automatique redisposée – kbfrPFsr'
+			echo      '   9  Afrique francophone disposition de clavier semi-automatique redisposée – kbfrAFsr'
+			read -p   '         ' re
+			case $re in
+				1) chemin="Variantes/kbfrFRs/kbfrFRs";;
+				2) chemin="Variantes/kbbrFRs/kbbrFRs";;
+				3) chemin="Variantes/kbfrPFs/kbfrPFs";;
+				4) chemin="Variantes/kbfrAFs/kbfrAFs";;
+				5) chemin="Variantes/kbfrBEs/kbfrBEs";;
+				6) chemin="Variantes/kbfrFRsr/kbfrFRsr";;
+				7) chemin="Variantes/kbbrFRsr/kbbrFRsr";;
+				8) chemin="Variantes/kbfrPFsr/kbfrPFsr";;
+				9) chemin="Variantes/kbfrAFsr/kbfrAFsr";;
+				*) chemin="nouvel-azerty";;
+			esac
+		;;
+	esac
+	echo -e "\n  ❓  Quelle sous-variante souhaitez-vous ? Pour passer, faites Entrée."
+	echo      "      o = Disposition ordinaire."
+	echo      "      w = Effacement arrière sur Windows droite."
+	echo      "     ws = Effacement arrière sur Windows droite sans Menu."
+	echo      "      m = Effacement arrière sur Menu, Menu sur Effacement arrière."
+	echo      "     ms = Effacement arrière sur Menu seulement, sans permutation."
+	echo      "      c = Permuter Effacement arrière et Contrôle droite."
+	echo      "      a = Permuter VerrCap et touche ISO pour claviers ANSI."
+	echo      "     am = Permuter VerrCap et touche ISO, et Effacement arrière et Menu."
+	echo      "    ams = Permuter VerrCap et touche ISO, Effacement arrière sur Menu."
+	read -p   "          o, w, ws, m, ms, c, a, am, ams ?   " re
+	case $re in
+		o)   suffixe="";;
+		w)   suffixe="-win";;
+		ws)  suffixe="-win-sans";;
+		m)   suffixe="-menu";;
+		ms)  suffixe="-menu-sans";;
+		c)   suffixe="-ctrl";;
+		a)   suffixe="-ansi";;
+		am)  suffixe="-ansi-menu";;
+		ams) suffixe="-ansi-menu-sans";;
+		*)   suffixe="";;
+	esac
+	chemin_complet="$chemin$suffixe.$mode"
+	xkbcomp activer/$chemin_complet :0
+	mkdir -p ~/.config/dispoclavier/activer
+	echo "$chemin_complet" > ~/.config/dispoclavier/activer/der.txt
+	cp activer/$chemin_complet ~/.config/dispoclavier/activer/der.$mode
+	echo -e "\n  ✅  La disposition de clavier vient d’être activée."
+	echo -e "\n     Son fichier .$mode et son chemin ont été sauvegardés"
+	echo      '         dans ~/.config/dispoclavier/activer/.'
+	echo -e "\n     Tous les retours d’expérience sont les bienvenus."
+	echo      '     S’il manque quoi que ce soit, ou à tout autre sujet relatif,'
+	echo      '     n’hésitez pas à créer un rapport de bogue :'
+	echo      '     https://github.com/dispoclavier/nouvel-azerty/issues'
+	echo -e "\n     N’hésitez pas non plus à lancer une discussion :"
+	echo      '     https://github.com/dispoclavier/nouvel-azerty/discussions'
+	echo -e "\n             Bonne utilisation !\n"
+}
+
+echo -e "\n    Bienvenue dans l’activateur de dispositions de clavier Dispoclavier."
+if [ -f "$HOME/.config/dispoclavier/activer/der.txt" ] && [ -d "activer" ]; then
+	chemin=$(<"$HOME/.config/dispoclavier/activer/der.txt");
+	if [ -f "activer/$chemin" ]; then
+		echo -e "\n  ❓  Souhaitez-vous activer la version actuelle de"
+		echo      '     la disposition dernièrement utilisée ?'
+		echo -e "\n       Pour l’activer, appuyez sur Entrée."
+		echo      '       Pour plus d’options, tapez o puis Entrée.'
+		echo      '       Pour quitter, tapez q ou n puis Entrée.'
+		read -p   '    ' reponse
+		case $reponse in
+			[nNqQ])
+				exit
+			;;
+			[oO])
+				options_de_disposition
+			;;
+			*)
+				config_xim
+				xkbcomp activer/$chemin :0
+				echo -e "\n  ✅  La disposition de clavier vient d’être activée."
+				echo -e "\n     Tous les retours d’expérience sont les bienvenus."
+				echo      '     S’il manque quoi que ce soit, ou à tout autre sujet relatif,'
+				echo      '     n’hésitez pas à créer un rapport de bogue :'
+				echo      '     https://github.com/dispoclavier/nouvel-azerty/issues'
+				echo -e "\n     N’hésitez pas non plus à lancer une discussion :"
+				echo      '     https://github.com/dispoclavier/nouvel-azerty/discussions'
+				echo -e "\n             Bonne utilisation !\n"
+			;;
+		esac
+	else
+		options_de_disposition
+	fi
+elif [ -f "$HOME/.config/dispoclavier/activer/der.$mode" ]; then
+	echo -e "\n  ❓  Souhaitez-vous réactiver la disposition dernièrement utilisée ?"
+	echo -e "\n       Pour la réactiver, appuyez sur Entrée."
+	echo      '       Pour plus d’options, tapez o puis Entrée.'
+	echo      '       Pour quitter, tapez q ou n puis Entrée.'
+	read -p   '    ' reponse
+	case $reponse in
+		[nNqQ])
+			exit
+		;;
+		[oO])
+			options_de_disposition
+		;;
+		*)
+			config_xim
+			xkbcomp ~/.config/dispoclavier/activer/der.$mode :0
+			echo -e "\n  ✅  La disposition de clavier vient d’être réactivée."
+			echo -e "\n     Tous les retours d’expérience sont les bienvenus."
+			echo      '     S’il manque quoi que ce soit, ou à tout autre sujet relatif,'
+			echo      '     n’hésitez pas à créer un rapport de bogue :'
+			echo      '     https://github.com/dispoclavier/nouvel-azerty/issues'
+			echo -e "\n     N’hésitez pas non plus à lancer une discussion :"
+			echo      '     https://github.com/dispoclavier/nouvel-azerty/discussions'
+			echo -e "\n             Bonne utilisation !\n"
+		;;
+	esac
+else
+	options_de_disposition
 fi
