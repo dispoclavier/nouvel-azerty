10130a28c833a66c95adc277a740205695f66773
dispoclavier <dispoclavier@orange.fr>
2025-07-14 20:28:20 +0200

    Update dispoled.c

--- linux-chromeos/dispoled.c
+++ linux-chromeos/dispoled.c
@@ -1,8 +1,8 @@
-//                       Date: 2023-12-02T2049+0100
+//                       Date: 2025-07-14T2014+0200
 //        Operating file name: dispoled
 //                   Encoding: UTF-8
 //                       Type: text/XKB configuration
-//                   Language: C-like
+//     Syntactic highlighting: C
 //                Description: Binding Scroll Lock indicator to Group 2
 //                   Platform: XKB with XCompose using OSes Linux and ChromeOS*
 //
@@ -12,16 +12,16 @@
 //                    Maxime : Simplifier la dactylographie, pas le français.
 //        English description: Keyboard layouts fit for national, regional and neighbor languages of Francophonie.
 //     Description française : Dispositions de clavier adaptées aux langues nationales, régionales et voisines de la Francophonie.
-//              Target public: General public and developers, journalists, scientists, secretaries, translators, typographers and writers
-//             Destinataires : Grand public et développeurs, écrivains, journalistes, scientifiques, secrétaires, traducteurs et typographes
+//              Target public: General public and developers, journalists, scientists, secretaries, translators, typographers and writers.
+//             Destinataires : Grand public et développeurs, écrivains, journalistes, scientifiques, secrétaires, traducteurs et typographes.
 //               Code licence: Apache 2.0
 //           Code licence URL: https://www.apache.org/licenses/LICENSE-2.0
 //           Non-code licence: CC-BY 4.0
 //       Non-code licence URL: https://creativecommons.org/licenses/by/4.0/deed.fr
-//              Email address: dev@dispoclavier.net
+//              Email address: dev[arobase]dispoclavier.com
 //
 //   * In Unicode CLDR, this platform is labeled ChromeOS, and it is explained that
-//     in the CLDR, *Linux* is supported alongside *ChromeOS*, or through ChromeOS.
+//     in the CLDR, Linux is supported alongside ChromeOS, or through ChromeOS.
 //
 //               Installation: 1. Add this file in /usr/share/X11/xkb/compat/
 //                             2. In /usr/share/X11/xkb/compat/complete
