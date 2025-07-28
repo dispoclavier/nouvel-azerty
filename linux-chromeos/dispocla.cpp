82e9d2491813bdd5b6fa34996bd8423d4e437765
dispoclavier <dispoclavier@orange.fr>
2025-07-28 22:37:33 +0200

    Update dispocla.cpp
    Disposition : Touches B08 et B10 :
    * Mode français :
    	* Permuter le point d’exclamation et le point-virgule espacés en Maj, et non espacés en AltFr et en Maj + AltFr, pour les remettre sur leurs touches historiques.
    	* Mettre le point médian "·" U00B7 sur touche vive en Minuscule sur B10 à la place de la contre-oblique "\\" U005C, qui est sur AltGr + R.
    	* En AltGr, laisser "§" U00A7 sur B10 pour la rétrocompatibilité, et "!" U0021 sur B08 pour la synergie avec "<" U003C, plutôt que d’y faire suivre ";" U003B.
    * Mode ASCII :
    	* Ne pas permuter "." U002E et ";" U003B, quitte à ne pas remettre le point-virgule sur sa place historique, afin d’assurer la stabilité du point, tout en ayant le point-virgule en Minuscule.
    	* En Maj, permuter "\\" U005C et "!" U0021 pour remettre le point d’exclamation sur sa touche historique.
    	* En AltFr, permuter "!" U0021 et ";" U003B pour la cohérence avec le mode français.
    	* En AltGr, ne pas permuter "!" U0021 et ";" U003B, pour la cohérence avec le mode français et la stabilité du point d’exclamation en AltGr.
    * En Maj + AltGr, ne pas permuter les touches mortes point souscrit et virgule souscrite, pour la cohérence avec "!" U0021 et ";" U003B (ou "§" U00A7) en AltGr, et parce que le point souscrit est plus fréquent que la virgule souscrite.

--- linux-chromeos/dispocla.cpp
+++ linux-chromeos/dispocla.cpp
@@ -1,4 +1,4 @@
-//                       Date: 2025-07-25T2035+0200
+//                       Date: 2025-07-28T2233+0200
 //        Operating file name: dispocla
 //                   Encoding: UTF-8
 //                       Type: text/XKB configuration
@@ -17,9 +17,13 @@
 //   * In Unicode CLDR, this platform is labeled ChromeOS, and it is explained that
 //     in the CLDR, Linux is supported alongside ChromeOS, or through ChromeOS.
 //
-//               Installation: 1. Add the file dispocla_perso.cpp in /usr/share/X11/xkb/symbols/dispocla_perso
+//               Installation: 1. Add the file dispocla_perso.cpp in
 //
-//                             2. Add this file in /usr/share/X11/xkb/symbols/dispocla
+//                                    /usr/share/X11/xkb/symbols/dispocla_perso
+//
+//                             2. Add this file in
+//
+//                                    /usr/share/X11/xkb/symbols/dispocla
 //
 //                             3. In /usr/share/X11/xkb/rules/evdev
 //                                comment out the rule:
@@ -1301,11 +1305,11 @@ xkb_symbols "kbfrFRs" {
 		[               comma,            question,            question,            question,        dead_cedilla,               UEF7B,          digitspace,              U1F49A ]
 	}; // UEF63 ' ?' spaced out with NNBSP; UEF7B '&#x'; U2063 INVISIBLE SEPARATOR; U1F499 💙 BLUE HEART 18th-ranking emoji; U1F49A 💚 GREEN HEART emoji
 
-	key <AB08> {
+	key <AB08> { // The inconsistency between index 3 on the one hand, and indices 4 and 6 on the other hand, is correct.
 		// Index:           1,                   2,                   3,                   4,                   5,                   6,                   7,                   8
-		[              period,               UEF60,              exclam,              exclam,       dead_belowdot,              exclam,              U1F631,              U1F4E3 ],
-		[              period,              exclam,              exclam,              exclam,       dead_belowdot,               UEF7A,          punctspace,              U1F4E2 ]
-	}; // UEF60 ' !' spaced out with NNBSP; UEF7A '&#'; U1F631 😱 FACE SCREAMING IN FEAR 2nd-top sad face emoji; U1F4E3 📣 CHEERING MEGAPHONE emoji; U1F4E2 📢 PUBLIC ADDRESS LOUDSPEAKER emoji
+		[              period,               UEF62,              exclam,           semicolon,       dead_belowdot,           semicolon,              U1F631,              U1F4E3 ],
+		[              period,           backslash,              exclam,           semicolon,       dead_belowdot,               UEF7A,          punctspace,              U1F4E2 ]
+	}; // UEF62 ' ;' spaced out with NNBSP; UEF7A '&#'; U1F631 😱 FACE SCREAMING IN FEAR 2nd-top sad face emoji; U1F4E3 📣 CHEERING MEGAPHONE emoji; U1F4E2 📢 PUBLIC ADDRESS LOUDSPEAKER emoji
 
 	key.type[Group1] = "EIGHT_LEVELS_PUNCTUATION_GROUP1";
 	key.type[Group2] = "EIGHT_LEVELS_PUNCTUATION_GROUP2";
@@ -1315,11 +1319,11 @@ xkb_symbols "kbfrFRs" {
 		[               colon,               slash,               colon,               colon,      dead_diaeresis,               U2044,              U1F612,               slash ]
 	}; // UEF61 ' :' spaced out with NNBSP; U2044 ⁄ FRACTION SLASH; U1F615 😕 CONFUSED FACE emoji; U1F612 😒 UNAMUSED FACE 3rd-top sad face emoji
 
-	key <AB10> {
+	key <AB10> { // The inconsistency between indices 3 and 4 is correct.
 		// Index:           1,                   2,                   3,                   4,                   5,                   6,                   7,                   8
-		[           backslash,               UEF62,             section,           semicolon,     dead_belowcomma,             section,              U1F494,           backslash ],
-		[           semicolon,           backslash,           semicolon,           semicolon,     dead_belowcomma,             section,              U1F609,           backslash ]
-	}; // UEF62 ' ;' spaced out with NNBSP; U1F494 💔 BROKEN HEART 16th-ranking emoji; U1F609 😉 WINKING FACE 24th-ranking emoji
+		[      periodcentered,               UEF60,             section,              exclam,     dead_belowcomma,             section,              U1F494,           backslash ],
+		[           semicolon,              exclam,           semicolon,              exclam,     dead_belowcomma,             section,              U1F609,           backslash ]
+	}; // UEF60 ' !' spaced out with NNBSP; U1F494 💔 BROKEN HEART 16th-ranking emoji; U1F609 😉 WINKING FACE 24th-ranking emoji
 
 	key.type[Group1] = "EIGHT_LEVELS_LEVEL1_CONTROL";
 	key.type[Group2] = "EIGHT_LEVELS_LEVEL1_CONTROL";
