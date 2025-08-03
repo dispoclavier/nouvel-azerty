e992a76d96e7071d3fa3f67ee8f56c6a50fd0374
dispoclavier <dispoclavier@orange.fr>
2025-08-03 04:09:12 +0200

    Update comp.sh

--- linux-chromeos/outils/comp.sh
+++ linux-chromeos/outils/comp.sh
@@ -2,18 +2,31 @@
 # 2023-01-14T1934+0100
 # 2023-12-02T2050+0100
 # 2024-08-26T0303+0200
-# = last modified.
+# 2025-08-03T0408+0200
+# == last modified.
 #
-# Generates all-in-one keymap files from X display. X display is the installed
-# and currently active keyboard layout. The recommended output format is `.xkb`
-# keymap source files, but `.xkm` compiled keymap file output is also supported
-# and can be activated by editing the de-facto boolean `human_readable` below.
-# https://www.ibm.com/docs/en/aix/7.3?topic=x-xkbcomp-command
+# Generates all-in-one keymap files from X display, the installed and currently
+# active keyboard layout.
 #
 # Requires to manually configure the subvariants in xkb/keycodes/evdev.
 #
 # Automatically switches layouts while compiling a subvariant as configured.
-# Courtesy https://askubuntu.com/questions/209597/how-do-i-change-keyboards-from-the-command-line
+# By courtesy of Stack Overflow.
+# https://askubuntu.com/questions/209597/how-do-i-change-keyboards-from-the-command-line
+#
+# The recommended output format is ".xkb" keymap source files, but the compiled
+# ".xkm" keymap file output is also supported. This can be activated by editing
+# the de-facto boolean "human_readable" below.
+#
+human_readable=1 # The integer 1 is used with "true" semantics.
+#
+# Rather than distributing compiled ".xkm" files, distributing merged ".xkb" is
+# preferable for transparency (the user is able to check what they got) and for
+# usability alike, since the user can directly customize the layout by making a
+# small edit right in the source file.
+#
+# The xkbcomp command is documented by IBM.
+# https://www.ibm.com/docs/en/aix/7.3?topic=x-xkbcomp-command
 #
 # For this to work, the keyboard layouts must be activated and set up in the
 # Gnome switch in the following order:
@@ -31,29 +44,31 @@
 # For these files to be usable in recent Linux distros, two key types that were
 # previously commented out are required to be uncommented, despite they are not
 # used in these keyboard layouts:
-# EIGHT_LEVEL_LEVEL_FIVE_LOCK and EIGHT_LEVEL_ALPHABETIC_LEVEL_FIVE_LOCK, as in
-# xkb/types/level5, required to prevent xkbcomp from throwing an error:
 #
-# X Error of failed request:  BadValue (integer parameter out of range for operation)
-#   Major opcode of failed request:  135 (XKEYBOARD)
-#   Minor opcode of failed request:  9 (XkbSetMap)
-#   Value in failed request:  0x135b0038
-#   Serial number of failed request:  170
-#   Current serial number in output stream:  176
+#     EIGHT_LEVEL_LEVEL_FIVE_LOCK
+#     EIGHT_LEVEL_ALPHABETIC_LEVEL_FIVE_LOCK
 #
-# Rather than distributing compiled `.xkm` files, distributing merged `.xkb` is
-# preferable for transparency (the user is able to check what they got) and for
-# usability alike, since the user can directly customize the layout by making a
-# small edit right in the all-in-one human readable source file.
+# These two types are defined in /usr/share/X11/xkb/types/level5.
 #
-human_readable=1 # The integer 1 is used with `true` semantics.
+# The issue with these two is that, when they are not defined, xkbcomp is prone
+# to returning an error of failed request.
+#
+#     X Error of failed request:  BadValue (integer parameter out of range for operation)
+#       Major opcode of failed request:  135 (XKEYBOARD)
+#       Minor opcode of failed request:  9 (XkbSetMap)
+#       Value in failed request:  0x135b0038
+#       Serial number of failed request:  170
+#       Current serial number in output stream:  176
+#
 
 if [ "$human_readable" -eq 1 ]; then
-	mode="xkb" # Used both as a flag and as the output file extension.
-	action="merged" # Used both as a dirname and in the confirmation.
+	# $mode is used both as a flag and as the output file extension.
+	mode="xkb"
+	# $action is used both as a dirname and in the confirmation message.
+	action="merged"
 else
-	mode="xkm" # Used both as a flag and as the output file extension.
-	action="compiled" # Used both as a dirname and in the confirmation.
+	mode="xkm"
+	action="compiled"
 fi
 
 function merge_or_compile {
@@ -112,6 +127,6 @@ merge_or_compile "kbfrPFsr" "7"
 merge_or_compile "kbfrAFsr" "8"
 merge_or_compile "kbfrFRsr" "0"
 
-# Replicate at root.
+# Copy the base layout to the folder’s root.
 cp kbfrFRs/kbfrFRs$suffix.$mode ../nouvel-azerty$suffix.$mode
 echo "nouvel-azerty$suffix.$mode mis à jour."
