617c9d8fb753d1b7ff96a59904185b46b78cf380
dispoclavier <dispoclavier@orange.fr>
2025-09-07 06:27:18 +0200

    Update kbdeadtrans.c

--- windows/kbdeadtrans.c
+++ windows/kbdeadtrans.c
@@ -5,16 +5,17 @@
 * 1985..2001 Microsoft Corporation pro parte
 * 2014..2025 Marcel Schneider dev[arobase]dispoclavier.com
 *
-* Support outline math brackets U27E6..  6.0.7.00.00 Wed 2025-09-03T0803+0200
+* History:
+* Outline brackets U27E6.. in !grave     6.0.7.00.00 Wed 2025-09-03T0803+0200
 * Improve Polynesian French variant      6.0.6.00.00 Tue 2025-09-02T1459+0200
-* Udpdate to Unicode 17.0                6.0.4.02.00 Wed 2025-08-27T2006+0200
+* Upgrade to Unicode 17.0                6.0.4.02.00 Wed 2025-08-27T2006+0200
 * Move DEADTRANS calls to kbdeadtrans.c  6.0.2.01.00 Tue 2025-08-19T1952+0200
 *
-* By courtesy of answers to:
+* By courtesy of respondents to:
 * https://stackoverflow.com/questions/11334371/cant-we-include-c-file
-\*****************************************************************************/
-
-/*****************************************************************************\
+*
+*
+*
 * Dead key array.
 *
 * On Windows, the dead key output is restricted to the low surrogate. An input
@@ -8333,3 +8334,4 @@ static ALLOC_SECTION_LDATA DEADKEY aDeadKey[] = {
 };
 
 // EOF
+
