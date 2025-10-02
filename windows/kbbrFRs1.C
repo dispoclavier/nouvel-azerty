9ce57caa34e4a5f42f42a6f3c2b50daeefb23487
dispoclavier <dispoclavier@orange.fr>
2025-10-02 17:40:05 +0200

    Update and rename kbbrFRs.C to kbbrFRs1.C

--- windows/kbbrFRs1.C
+++ windows/kbbrFRs1.C
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* Module Name: kbbrFRs.C
+* Module Name: kbbrFRs1.C
 *
 * Keyboard layout C source.
 *
@@ -7,6 +7,7 @@
 * Copyright (c) 2014-2025, Marcel Schneider dev[arobase]dispoclavier.com
 *
 * History:
+* Rename kbbrFRs.C to kbbrFRs1.C           6.0.9.8.0 Thu 2025-10-02T1446+0200
 * Move alloc table to kbbrFRs-allo.c       6.0.8.0.0 Thu 2025-09-25T1835+0200
 * Move variant ID to UX level 4            6.0.7.3.0 Sun 2025-09-21T1332+0200
 * Move kbdeadtrans.c #inc to kbcommon.c    6.0.7.2.0 Tue 2025-09-16T0311+0200
@@ -54,16 +55,16 @@ static ALLOC_SECTION_LDATA LIGATURE16 aLigature[] = {
 #include "kbcommon-3.c"
 
 /*****************************************************************************\
-* Note on kbbrFRs.DEF
+* Note on kbbrFRs1.DEF
 *
 * The .DEF file is not provided. The Keyboard Table Generation Tool (Unicode)
-* generates a .DEF file on the fly while transpiling kbbrFRs.klc right before
+* generates a .DEF file on the fly while transpiling kbbrFRs1.klc right before
 * giving the sources to the compiler. So, no need to support the .DEF file as
 * a part of the source code.
 *
-* For reference, this is the content of kbbrFRs.DEF:
+* For reference, this is the content of kbbrFRs1.DEF:
 
-LIBRARY kbbrFRs
+LIBRARY kbbrFRs1
  
  EXPORTS 
     KbdLayerDescriptor @1
