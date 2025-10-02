79c40bad0737b1a1b1104ec52c484650bb5d9d8c
dispoclavier <dispoclavier@orange.fr>
2025-10-02 18:18:19 +0200

    Update and rename kbfrFRsr.C to kbfrFRr1.C

--- windows/redispo/kbfrFRr1.C
+++ windows/redispo/kbfrFRr1.C
@@ -1,5 +1,5 @@
 /*****************************************************************************\
-* Module Name: kbfrFRsr.C
+* Module Name: kbfrFRr1.C
 *
 * Keyboard layout C source.
 *
@@ -7,6 +7,7 @@
 * Copyright (c) 2014-2025, Marcel Schneider dev[arobase]dispoclavier.com
 *
 * History:
+* Rename kbfrFRsr.C to kbfrFRr1.C          6.0.9.8.0 Thu 2025-10-02T1439+0200
 * Move alloc table to kbfrFRsr-allo.c      6.0.8.0.0 Thu 2025-09-25T1836+0200
 * Move variant ID to UX level 4            6.0.7.3.0 Sun 2025-09-21T1332+0200
 * Move kbdeadtrans.c #inc to kbcommon.c    6.0.7.2.0 Tue 2025-09-16T0311+0200
@@ -116,7 +117,7 @@
 #define ALLOC_SECTION_LDATA
 #endif
 
-#include "kbfrFRsr-allo.c"
+#include "kbfrFRr-allo.c"
 
 static ALLOC_SECTION_LDATA LIGATURE16 aLigature[] = {
 
@@ -139,16 +140,16 @@ static ALLOC_SECTION_LDATA LIGATURE16 aLigature[] = {
 #include "kbcommon-3.c"
 
 /*****************************************************************************\
-* Note on kbfrFRsr.DEF
+* Note on kbfrFRr1.DEF
 *
 * The .DEF file is not provided. The Keyboard Table Generation Tool (Unicode)
-* generates a .DEF file on the fly while transpiling kbfrFRsr.klc right before
+* generates a .DEF file on the fly while transpiling kbfrFRr1.klc right before
 * giving the sources to the compiler. So, no need to support the .DEF file as
 * a part of the source code.
 *
-* For reference, this is the content of kbfrFRsr.DEF:
+* For reference, this is the content of kbfrFRr1.DEF:
 
-LIBRARY kbfrFRsr
+LIBRARY kbfrFRr1
  
  EXPORTS 
     KbdLayerDescriptor @1
