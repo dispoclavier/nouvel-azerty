71edaea623bb75946c19cbef29408d5195d702c3
dispoclavier <dispoclavier@orange.fr>
2025-10-02 21:34:37 +0200

    6.0.9.9
    Fournir des clones de dépannage.

---
+++ windows/kbfrAFs2.C
@@ -0,0 +1,65 @@
+/*****************************************************************************\
+* Module Name: kbfrAFs2.C
+*
+* Keyboard layout C source.
+*
+* Copyright (c) 1985-2001, Microsoft Corporation
+* Copyright (c) 2014-2025, Marcel Schneider dev[arobase]dispoclavier.com
+*
+* History:
+* Update variant descriptor string output  6.0.9.9.0 Thu 2025-10-02T1929+0200
+* Copy from kbfrAFs1.C                     6.0.9.9.0 Thu 2025-10-02T1857+0200
+*
+\*****************************************************************************/
+
+#include <windows.h>
+#include "kbazerty.h"
+#include "kbcommon.h"
+
+#if defined(_M_IA64)
+#pragma section(".data")
+#define ALLOC_SECTION_LDATA __declspec(allocate(".data"))
+#else
+#pragma data_seg(".data")
+#define ALLOC_SECTION_LDATA
+#endif
+
+#include "kbfrAFs-allo.c"
+
+static ALLOC_SECTION_LDATA LIGATURE16 aLigature[] = {
+
+  {VK_ESCAPE ,3 ,' ' ,'f' ,'r' ,'_' ,'A' ,'F' ,'_' ,'s' ,'e' ,'m' ,'i' ,'_' ,'_' ,'b' ,'i' ,'s' },
+	// The first code unit of long multiunit ligatures may not be output, hence the leading space.
+
+  #include "kbligazerty.c"
+  #include "kbligatures.c"
+
+  {0         ,0 ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   }
+};
+
+/*****************************************************************************\
+* Final include
+*
+* The file kbcommon-3.c needs to be included last because it contains the
+* return statement.
+*
+\*****************************************************************************/
+#include "kbcommon-3.c"
+
+/*****************************************************************************\
+* Note on kbfrAFs2.DEF
+*
+* The .DEF file is not provided. The Keyboard Table Generation Tool (Unicode)
+* generates a .DEF file on the fly while transpiling kbfrAFs2.klc right before
+* giving the sources to the compiler. So, no need to support the .DEF file as
+* a part of the source code.
+*
+* For reference, this is the content of kbfrAFs2.DEF:
+
+LIBRARY kbfrAFs2
+ 
+ EXPORTS 
+    KbdLayerDescriptor @1
+
+
+\*****************************************************************************/
---
+++ windows/kbfrPFs2.C
@@ -0,0 +1,65 @@
+/*****************************************************************************\
+* Module Name: kbfrPFs2.C
+*
+* Keyboard layout C source.
+*
+* Copyright (c) 1985-2001, Microsoft Corporation
+* Copyright (c) 2014-2025, Marcel Schneider dev[arobase]dispoclavier.com
+*
+* History:
+* Update variant descriptor string output  6.0.9.9.0 Thu 2025-10-02T1930+0200
+* Copy from kbfrPFs1.C                     6.0.9.9.0 Thu 2025-10-02T1857+0200
+*
+\*****************************************************************************/
+
+#include <windows.h>
+#include "kbazerty.h"
+#include "kbcommon.h"
+
+#if defined(_M_IA64)
+#pragma section(".data")
+#define ALLOC_SECTION_LDATA __declspec(allocate(".data"))
+#else
+#pragma data_seg(".data")
+#define ALLOC_SECTION_LDATA
+#endif
+
+#include "kbfrPFs-allo.c"
+
+static ALLOC_SECTION_LDATA LIGATURE16 aLigature[] = {
+
+  {VK_ESCAPE ,3 ,' ' ,'f' ,'r' ,'_' ,'P' ,'F' ,'_' ,'s' ,'e' ,'m' ,'i' ,'_' ,'_' ,'b' ,'i' ,'s' },
+	// The first code unit of long multiunit ligatures may not be output, hence the leading space.
+
+  #include "kbligazerty.c"
+  #include "kbligatures.c"
+
+  {0         ,0 ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   ,0   }
+};
+
+/*****************************************************************************\
+* Final include
+*
+* The file kbcommon-3.c needs to be included last because it contains the
+* return statement.
+*
+\*****************************************************************************/
+#include "kbcommon-3.c"
+
+/*****************************************************************************\
+* Note on kbfrPFs2.DEF
+*
+* The .DEF file is not provided. The Keyboard Table Generation Tool (Unicode)
+* generates a .DEF file on the fly while transpiling kbfrPFs2.klc right before
+* giving the sources to the compiler. So, no need to support the .DEF file as
+* a part of the source code.
+*
+* For reference, this is the content of kbfrPFs2.DEF:
+
+LIBRARY kbfrPFs2
+ 
+ EXPORTS 
+    KbdLayerDescriptor @1
+
+
+\*****************************************************************************/
